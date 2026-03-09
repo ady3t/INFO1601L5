0bacd785c17f14d3a0b3ebccfdf502ff1c1c81a4
ady3t <adyett53@gmail.com>
2026-03-09 10:57:37 -0400

    task 3 - Primitive Data Types & Strings

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+//let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];// 10 elements
+//for(let i=0; i< arr.length; i++){
+// console.log(arr[i])
+//}
 
-for(let i=0; i< arr.length; i++){
-  console.log(arr[i])
-}
+//let radius = 7;
+//const pi = 3.14;
+//let area = radius * radius * pi;
 
-let radius = 7;
-const pi = 3.14;
+let name = "bob";
+let age = 24;
 
-let area = radius * radius * pi;
+console.log(typeof(name))//string
+console.log(typeof(age))//number
+
+console.log(`Hello my name is ${name}, I'm ${age} years old`);
+//Hello my name is bob, I'm 24 years old.
+
+console.log(`I was born in ${2020 - age}`)
+//I was born in 1966
