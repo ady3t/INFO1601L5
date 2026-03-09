1d54bdc77de11cad47b1dcef2673cf99c101a209
ady3t <adyett53@gmail.com>
2026-03-09 14:30:56 -0400

    task 6

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-let person = {
-  // key : value
-  "name": "John",
-  "age": 12
-};
+//Create a constructor a functions which builds object for us
+function createPerson(name, height, weight) {
+  return { name: name, height: height, weight: weight };
+}
 
-//we can print the values of a key in two ways
-console.log(person["age"]);//Using an index
-console.log(person.name);//referencing the key as a property
+function calcBMI(weight, height) {
+  return weight / (height * height);
+}
 
-person["weight"] = 70;
-
-person.marks = [67, 34, 55, 89];
-
-let firstMark = person.marks[0];
-
-//mixing array and objects
-let people = [
-  person,
-  {
-    name: "Jane",
-    age: 23,
-    marks: [51, 78, 99, 76]
+function avgBMI(people) {
+  let sum = 0;
+  for (let person of people) {
+    //sum the bmi of each person
+    sum += calcBMI(person.weight, person.height);
   }
+  //calculate average
+  return sum / people.length;
+}
+
+//create a collection of people
+let people = [
+  createPerson("Sally", 60, 2.5),
+  createPerson("Ben", 81, 3),
+  createPerson("Shelly", 50, 1.7)
 ];
 
-let johnClone = {};//empty object
-
-//copying objects
-Object.assign(johnClone, person);
-
-console.log(johnClone);
-
-
-console.log(people[0].marks[0]);//what is printed?
+console.log(avgBMI(people));
