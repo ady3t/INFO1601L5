6187c1abd94b370f2a7f485004d47b1e3d061483
ady3t <adyett53@gmail.com>
2026-03-09 13:55:43 -0400

    task 5.1

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-function happyPrint(string){
- console.log("😀: "+string);
-}
+let arr = [ -5, 16, 33, 42, 103, 344];
 
-function sadPrint(string){
- console.log("😢: "+string);
-}
+console.log(arr.includes(-5));//true
 
-//This high-order function adds 2 parameters and passes the answer to the callback
-function add(a, b, callback){
-   let ans = a + b;
-   callback(ans);// call the callback and pass the answer to it
-}
+arr.push(11);//adds item to the end
 
-//call add passing the callbacks to it
+console.log(arr)//[ -5, 16, 33, 42, 103, 344, 11];
 
-add(5, 10, happyPrint);
-add(11, 12, sadPrint);
+let lastItem == arr.pop();//removes last item
 
-//prints current date
-function printDate(){
-   console.log(new Date().toLocaleTimeString());
-}
-setTimeout(printDate, 2000);      
-setInterval(printDate, 1000);
+console.log(lastItem, arr);//11, [ -5, 16, 33, 42, 103, 344]
+
+arr.unshift(22);//adds item to the front
+
+console.log(arr);//[22, -5, 16, 33, 42, 103, 344]
+
+let firstItem = arr.shift();//removes first item
+console.log(firstItem, arr);//22, [-5, 16, 33, 42, 103, 344]
+
+let reversed = arr.reverse();//creates a new array in reverse order 
+console.log(reversed);//[344, 103, 42, 33, 16, -5]
+console.log(arr.join('-'));//"-5-103-16-33-344-42" joins array with provided separator
